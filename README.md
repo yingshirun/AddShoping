a493b19198aca7567d820eb95679c9c24fb72178
yingshirun <649486270@qq.com>
2016-07-20 15:36:26 +0800

    修改readme

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # AddShoping
-添加商品到购物车的动画效果
+添加商品到购物车的动画效果,并且支持GridView
 ##效果图
 ![image]
 (https://github.com/yingshirun/AddShoping/blob/master/gifimg/jdfw.gif)
--- app/src/main/java/com/addshoping/MainActivity.java
+++ app/src/main/java/com/addshoping/MainActivity.java
@@ -22,13 +22,14 @@ import android.widget.ImageView;
 import android.widget.ListView;
 
 import com.addshoping.GoodAdapter.HolderClickListener;
+
 /**
  *
  *
  */
 public class MainActivity extends Activity {
     private ListView listView;
-//    private GridView listView;
+    //    private GridView listView;
     private Button cart_btn;
     private GoodAdapter goodAdapter;
     //动画时间
@@ -38,14 +39,14 @@ public class MainActivity extends Activity {
     //是否完成清理
     private boolean isClean = false;
     private FrameLayout animation_viewGroup;
-    private Handler myHandler = new Handler(){
-        public void handleMessage(Message msg){
-            switch(msg.what){
+    private Handler myHandler = new Handler() {
+        public void handleMessage(Message msg) {
+            switch (msg.what) {
                 case 0:
                     //用来清除动画后留下的垃圾
-                    try{
+                    try {
                         animation_viewGroup.removeAllViews();
-                    }catch(Exception e){
+                    } catch (Exception e) {
 
                     }
                     isClean = false;
@@ -56,54 +57,55 @@ public class MainActivity extends Activity {
             }
         }
     };
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 //        listView = (GridView)this.findViewById(R.id.listview);
-        listView = (ListView)this.findViewById(R.id.listview);
-        cart_btn = (Button)this.findViewById(R.id.button);
+        listView = (ListView) this.findViewById(R.id.listview);
+        cart_btn = (Button) this.findViewById(R.id.button);
         animation_viewGroup = createAnimLayout();
         goodAdapter = new GoodAdapter(this);
 
-        goodAdapter.SetOnSetHolderClickListener(new HolderClickListener(){
+        goodAdapter.SetOnSetHolderClickListener(new HolderClickListener() {
 
             @Override
-            public void onHolderClick(Drawable drawable,int[] start_location) {
+            public void onHolderClick(Drawable drawable, int[] start_location) {
                 // TODO Auto-generated method stub
-                doAnim(drawable,start_location);
+                doAnim(drawable, start_location);
             }
 
         });
         listView.setAdapter(goodAdapter);
     }
 
-    private void doAnim(Drawable drawable,int[] start_location){
-        if(!isClean){
-            setAnim(drawable,start_location);
-        }else{
-            try{
+    private void doAnim(Drawable drawable, int[] start_location) {
+        if (!isClean) {
+            setAnim(drawable, start_location);
+        } else {
+            try {
                 animation_viewGroup.removeAllViews();
                 isClean = false;
-                setAnim(drawable,start_location);
-            }catch(Exception e){
+                setAnim(drawable, start_location);
+            } catch (Exception e) {
                 e.printStackTrace();
-            }
-            finally{
+            } finally {
                 isClean = true;
             }
         }
     }
+
     /**
-     * @Description: 创建动画层
      * @param
      * @return void
      * @throws
+     * @Description: 创建动画层
      */
-    private FrameLayout createAnimLayout(){
-        ViewGroup rootView = (ViewGroup)this.getWindow().getDecorView();
+    private FrameLayout createAnimLayout() {
+        ViewGroup rootView = (ViewGroup) this.getWindow().getDecorView();
         FrameLayout animLayout = new FrameLayout(this);
-        FrameLayout.LayoutParams lp = new FrameLayout.LayoutParams(FrameLayout.LayoutParams.MATCH_PARENT,FrameLayout.LayoutParams.MATCH_PARENT);
+        FrameLayout.LayoutParams lp = new FrameLayout.LayoutParams(FrameLayout.LayoutParams.MATCH_PARENT, FrameLayout.LayoutParams.MATCH_PARENT);
         animLayout.setLayoutParams(lp);
         animLayout.setBackgroundResource(android.R.color.transparent);
         rootView.addView(animLayout);
@@ -112,21 +114,18 @@ public class MainActivity extends Activity {
     }
 
     /**
-     * @deprecated 将要执行动画的view 添加到动画层
-     * @param vg
-     *        动画运行的层 这里是frameLayout
-     * @param view
-     *        要运行动画的View
-     * @param location
-     *        动画的起始位置
+     * @param vg       动画运行的层 这里是frameLayout
+     * @param view     要运行动画的View
+     * @param location 动画的起始位置
      * @return
+     * @deprecated 将要执行动画的view 添加到动画层
      */
-    private View addViewToAnimLayout(ViewGroup vg,View view,int[] location){
+    private View addViewToAnimLayout(ViewGroup vg, View view, int[] location) {
         int x = location[0];
         int y = location[1];
         vg.addView(view);
         FrameLayout.LayoutParams lp = new FrameLayout.LayoutParams(
-                dip2px(this,90),dip2px(this,90));
+                dip2px(this, 90), dip2px(this, 90));
         lp.leftMargin = x;
         lp.topMargin = y;
         view.setPadding(5, 5, 5, 5);
@@ -134,43 +133,45 @@ public class MainActivity extends Activity {
 
         return view;
     }
+
     /**
      * dip，dp转化成px 用来处理不同分辨路的屏幕
+     *
      * @param context
      * @param dpValue
      * @return
      */
-    private int dip2px(Context context,float dpValue){
+    private int dip2px(Context context, float dpValue) {
         float scale = context.getResources().getDisplayMetrics().density;
-        return (int)(dpValue*scale +0.5f);
+        return (int) (dpValue * scale + 0.5f);
     }
 
     /**
      * 动画效果设置
-     * @param drawable
-     *       将要加入购物车的商品
-     * @param start_location
-     *        起始位置
+     *
+     * @param drawable       将要加入购物车的商品
+     * @param start_location 起始位置
      */
-    @SuppressLint("NewApi") private void setAnim(Drawable drawable,int[] start_location){
+    @SuppressLint("NewApi")
+    private void setAnim(Drawable drawable, int[] start_location) {
 
 
-        Animation mScaleAnimation = new ScaleAnimation(1.5f,0.0f,1.5f,0.0f,Animation.RELATIVE_TO_SELF,0.1f,Animation.RELATIVE_TO_SELF,0.1f);
+        Animation mScaleAnimation = new ScaleAnimation(1.5f, 0.0f, 1.5f, 0.0f, Animation.RELATIVE_TO_SELF, 0.1f, Animation.RELATIVE_TO_SELF, 0.1f);
         mScaleAnimation.setDuration(AnimationDuration);
         mScaleAnimation.setFillAfter(true);
 
 
         final ImageView iview = new ImageView(this);
         iview.setImageDrawable(drawable);
-        final View view = addViewToAnimLayout(animation_viewGroup,iview,start_location);
+        final View view = addViewToAnimLayout(animation_viewGroup, iview, start_location);
         view.setAlpha(0.6f);
 
         int[] end_location = new int[2];
         cart_btn.getLocationInWindow(end_location);
         int endX = end_location[0] - start_location[0];
-        int endY = end_location[1]-start_location[1];
+        int endY = end_location[1] - start_location[1];
 
-        Animation mTranslateAnimation = new TranslateAnimation(0,endX,0,endY);
+        Animation mTranslateAnimation = new TranslateAnimation(0, endX, 0, endY);
         Animation mRotateAnimation = new RotateAnimation(0, 180, Animation.RELATIVE_TO_SELF, 0.5f, Animation.RELATIVE_TO_SELF, 0.5f);
         mRotateAnimation.setDuration(AnimationDuration);
         mTranslateAnimation.setDuration(AnimationDuration);
@@ -181,7 +182,7 @@ public class MainActivity extends Activity {
         mAnimationSet.addAnimation(mScaleAnimation);
         mAnimationSet.addAnimation(mTranslateAnimation);
 
-        mAnimationSet.setAnimationListener(new AnimationListener(){
+        mAnimationSet.setAnimationListener(new AnimationListener() {
 
             @Override
             public void onAnimationStart(Animation animation) {
@@ -194,7 +195,7 @@ public class MainActivity extends Activity {
                 // TODO Auto-generated method stub
 
                 number--;
-                if(number==0){
+                if (number == 0) {
                     isClean = true;
                     myHandler.sendEmptyMessage(0);
                 }
@@ -211,6 +212,7 @@ public class MainActivity extends Activity {
         view.startAnimation(mAnimationSet);
 
     }
+
     /**
      * 内存过低时及时处理动画产生的未处理冗余
      */
@@ -218,9 +220,9 @@ public class MainActivity extends Activity {
     public void onLowMemory() {
         // TODO Auto-generated method stub
         isClean = true;
-        try{
+        try {
             animation_viewGroup.removeAllViews();
-        }catch(Exception e){
+        } catch (Exception e) {
             e.printStackTrace();
         }
         isClean = false;
